2360994cd79becac4833892f7d9c1b645168d214
YahyaMezzi <mezz00216@gmail.com>
2020-04-27 12:04:18 +0100

    UI implementation

---
+++ src/main/Main.java
@@ -0,0 +1,48 @@
+package main;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../views/start.fxml"));
+        //for a borderless window
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setScene(new Scene(root, 507, 389));
+        primaryStage.show();
+
+        //make the window movable
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        root.setOnMouseDragged(new EventHandler<>() {
+            @Override
+            public void handle(MouseEvent event) {
+                primaryStage.setX(event.getScreenX() - xOffset);
+                primaryStage.setY(event.getScreenY() - yOffset);
+            }
+        });
+
+    }
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
